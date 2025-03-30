4a939e41979e3e601849faccaa68ddb21424d2c9
Gustavo de Freitas Andrade <160246041+GGuFreitas@users.noreply.github.com>
2025-03-30 15:11:53 -0300

    Função limpar historico
    função de limpar historico está funcionando

--- modules/historico.py
+++ modules/historico.py
@@ -35,3 +35,11 @@ def carregar_historico():
         with open(HISTORICO_ARQUIVO, "r") as f:
             return json.load(f)
     return []
+
+def limpar_historico():
+    """Remove todo o histórico de buscas do arquivo JSON."""
+    if os.path.exists(HISTORICO_ARQUIVO):
+        with open(HISTORICO_ARQUIVO, "w") as f:
+            json.dump([], f, indent=4)  # Substitui o conteúdo por uma lista vazia
+        return True
+    return False
